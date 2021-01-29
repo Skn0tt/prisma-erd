38b0f07d4b43234c46750d788f804a6ab7362980
Simon Knott <info@simonknott.de>
2021-01-29 16:46:34 +0100

    Use @prisma-engines

---
+++ api/parse-dml.js
@@ -0,0 +1,34 @@
+import * as path from "path";
+import * as child_process from "child_process";
+
+const engineBasePath = path.resolve(
+  __dirname,
+  "..",
+  "node_modules",
+  "@prisma",
+  "engines"
+);
+
+const engine = path.join(engineBasePath, `query-engine-${process.platform}`);
+
+async function parseDatamodel(model) {
+  const modelB64 = Buffer.from(model).toString("base64");
+
+  const parsed = await new Promise((resolve) => {
+    const process = child_process.exec(
+      `${engine} --datamodel=${modelB64} cli dmmf`
+    );
+    let output = "";
+    process.stdout.on("data", (d) => (output += d));
+    process.on("exit", () => {
+      resolve(output);
+    });
+  });
+
+  return parsed;
+}
+
+export default async (req, res) => {
+  const datamodel = await parseDatamodel(req.body);
+  return res.status(200).send(datamodel);
+};
